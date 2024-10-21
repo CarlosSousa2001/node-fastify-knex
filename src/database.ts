0be68476866dbf30aa0d02abb264fbdcf255c4fe
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-10-21 15:35:19 -0300

    feature:knex: configurações inciais variaveis de ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
+import "dotenv/config"
 import { knex as setupKnex, Knex } from "knex"
 
 export const config: Knex.Config = {
     client: "sqlite3",
     connection: {
-        filename: "./db/app.db",
+        filename: process.env.DATABASE_URL as string,
     },
     useNullAsDefault: true,
     migrations: {
