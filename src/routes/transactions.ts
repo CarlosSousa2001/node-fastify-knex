4ef82ca08420fd6d08f701199223fee6e561d8f2
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-10-21 17:08:07 -0300

    feature/routes - endpoint buscar todas transações - buscar por ID

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,42 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
 import { knex } from "../database"
 import { randomUUID } from "node:crypto"
-import {z} from "zod"
+import { z } from "zod"
 
-export async function transactionsRoutes(app: FastifyInstance){
+export async function transactionsRoutes(app: FastifyInstance) {
 
-    app.post("/", async function handler (request: FastifyRequest, reply: FastifyReply) {
+
+    app.get("/", async () => {
+        const transactions = await knex("transactions").select()
+
+        return {
+            transactions
+        }
+    })
+
+
+    app.get("/:id", async (request: FastifyRequest, reply: FastifyReply) => {
+
+        const getTrasacntionParamsSchema = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = getTrasacntionParamsSchema.parse(request.params)
+
+        const transaction = await knex("transactions").where("id", id).first()
+
+        if (!transaction) {
+            return {
+                message: "transaction not found"
+            }
+        }
+
+        return {
+            transaction
+        }
+    })
+
+    app.post("/", async function handler(request: FastifyRequest, reply: FastifyReply) {
 
         const createTransactionsBodySchema = z.object({
             title: z.string(),
@@ -13,14 +44,14 @@ export async function transactionsRoutes(app: FastifyInstance){
             type: z.enum(["credit", "Debit"])
         })
 
-        const {title, amount, type} = createTransactionsBodySchema.parse(request.body)
+        const { title, amount, type } = createTransactionsBodySchema.parse(request.body)
 
         const transaction = await knex("transactions").insert({
-          id: randomUUID(),
-          title: title,
-          amount: type === "credit" ? amount : amount * -1,
+            id: randomUUID(),
+            title: title,
+            amount: type === "credit" ? amount : amount * -1,
         })
-    
+
         return reply.status(201).send
     })
 
